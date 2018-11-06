9f3666d6bd9e509e31ac9f093f917337f58299d9
urvaius <urvaius@gmail.com>
2018-11-06 14:02:06 -0800

    npm setup and nodemon

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-var app = express();
 
+const app = express();
+const port = process.env.PORT || 3000;
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
-app.use('/css', express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js')));
-app.use('/js', express.static(path.join(__dirname,'/node/modules/jquery/dist')));
-app.use('/js', express.static(path.join(__dirname,"/node_modules/popper.js/dist")));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node/modules/jquery/dist')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist')));
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(3000, function(){
-    debug(`listening on port ${chalk.green('3000')}`);
+app.listen(port, () => {
+  debug(`listening at port ${chalk.green(port)}`);
 });
 
-//$env:DEBUG = "*";node app.js
-//$env:DEBUG = "app";node app.js
+// $env:DEBUG = "*";node app.js
+// $env:DEBUG = "app";node app.js
